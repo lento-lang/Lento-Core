831b67bcfec10c7479a62864adc12916ee0b8396
William Rågstad <william.ragstad@gmail.com>
2023-01-03 23:16:15 +0100

    Add skeleton structure for parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,39 @@
+use std::path::Path;
+
 pub mod conf;
+
+#[derive(Debug, Clone)]
+pub enum AstNode {
+    Root(Ast),
+    Print(String),
+}
+#[derive(Debug, Clone)]
+pub struct Ast {
+    pub name: String,
+    pub value: Box<AstNode>
+}
+
+#[derive(Debug, Clone)]
+pub struct ParseSuccess<'a> {
+    pub source_file: &'a Path,
+    pub ast: Ast
+}
+#[derive(Debug, Clone)]
+pub struct  ParseFail<'a> {
+    pub source_file: &'a Path,
+    pub msg: String
+}
+
+pub fn parse_file(file: &Path) -> Result<ParseSuccess, ParseFail> {
+    Ok(ParseSuccess {
+        source_file: file,
+        ast: Ast { name: "root".to_string(), value: Box::new(AstNode::Print("Hello, World!".to_string())) }
+    })
+}
+
+pub fn interpret_file(file: &Path) -> Result<(), (u8, String)> {
+    Ok(())
+}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
