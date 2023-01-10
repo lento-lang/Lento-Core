7cb7643204acf95d021c55cb623b62e52a49d721
William Rågstad <william.ragstad@gmail.com>
2023-01-10 22:37:32 +0100

    Update interpreter.rs

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -3,6 +3,7 @@ use crate::parser::ast::{Ast, AstNode};
 pub fn interpret_ast(ast: &Ast) -> Result<(), (u8, String)> {
     match ast.value.as_ref() {
         AstNode::Print(msg) => println!("{}", msg),
+        AstNode::Unit => (),
         _ => todo!("Implement other AST nodes")
     }
     Ok(())
