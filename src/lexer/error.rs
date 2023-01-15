32823d63a2f561aafead88c05f2caecd6da174b5
William Rågstad <william.ragstad@gmail.com>
2023-01-15 14:41:10 +0100

    Refactor Lexer error

--- src/lexer/error.rs
+++ src/lexer/error.rs
@@ -1,10 +1,28 @@
+use super::token::LineInfoSpan;
+
 // Lexer error
 pub struct LexerError {
     pub message: String,
-    pub index_start: usize,
-    pub index_end: usize,
-    pub line_start: usize,
-    pub line_end: usize,
-    pub column_start: usize,
-    pub column_end: usize,
+    pub info: LineInfoSpan,
+}
+
+impl LexerError {
+    pub fn new(message: String, info: LineInfoSpan) -> Self {
+        Self {
+            message,
+            info
+        }
+    }
+
+    pub fn UnexpectedEndOfFile(info: LineInfoSpan) -> Self {
+        Self::new("Unexpected end of file".to_string(), info)
+    }
+
+    pub fn UnexpectedCharacter(c: char, info: LineInfoSpan) -> Self {
+        Self::new(format!("Unexpected character '{}'", c), info)
+    }
+
+    pub fn InvalidChar(c: String, info: LineInfoSpan) -> Self {
+        Self::new(format!("Invalid character literal '{}'", c), info)
+    }
 }
