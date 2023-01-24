4d525abeedfa33de21ca42a4b52d3833640c1ce3
William Rågstad <william.ragstad@gmail.com>
2023-01-24 20:59:05 +0100

    Initialize standard library

---
+++ src/stdlib/init.rs
@@ -0,0 +1,111 @@
+use std::io::{BufRead, Seek};
+
+use crate::{lexer::op::{Operator, RuntimeOperator, StaticOperator, OperatorAssociativity, StaticOperatorHandler, RuntimeOperatorHandler, StaticOperatorAst, OperatorPosition}, parser::ast::Ast, interpreter::{value::{FunctionVariation, NativeFunctionParameters, Value, Number, SignedInteger, Function}, error::{runtime_error, RuntimeError}, environment::Environment}, type_checker::types::{Type, FunctionParameterType, std_primitive_types, GetType}, util::str::Str};
+
+use super::super::lexer::lexer::Lexer;
+
+
+const TY_ANY: Type = std_primitive_types::ANY;
+const TY_INT: Type = std_primitive_types::INT32;
+const TY_UNIT: Type = std_primitive_types::UNIT;
+
+//--------------------------------------------------------------------------------------//
+//                                                                                      //
+//                                      Init Lexer                                      //
+//                                                                                      //
+//--------------------------------------------------------------------------------------//
+
+
+pub fn init_lexer<R: BufRead + Seek>(lexer: &mut Lexer<R>) {
+
+//--------------------------------------------------------------------------------------//
+//                                       Helpers                                        //
+//--------------------------------------------------------------------------------------//
+
+    let add_static = |lexer: &mut Lexer<R>, name: &str, sym: &str, pos: OperatorPosition, prec: u16, assoc: OperatorAssociativity, overloadable: bool, handler: StaticOperatorHandler| {
+        lexer.define_op(Operator::Static(StaticOperator::new_str(name, sym, pos, prec, assoc, overloadable, handler)));
+    };
+    let add_runtime = |lexer: &mut Lexer<R>, name: &str, sym: &str, pos: OperatorPosition, prec: u16, assoc: OperatorAssociativity, overloadable: bool, handler: RuntimeOperatorHandler| {
+        lexer.define_op(Operator::Runtime(RuntimeOperator::new_str(name, sym, pos, prec, assoc, overloadable, handler)));
+    };
+
+//--------------------------------------------------------------------------------------//
+//                               Native Static Functions                                //
+//--------------------------------------------------------------------------------------//
+
+fn assign_handler(op: StaticOperatorAst) -> Ast {
+    if let StaticOperatorAst::Infix(lhs, rhs) = op {
+        Ast::Assignment(Box::new(lhs), Box::new(rhs), None)
+    } else {
+        panic!("assign_handler() expects an infix operator")
+    }
+}
+
+//--------------------------------------------------------------------------------------//
+//                               Native Runtime Operators                               //
+//--------------------------------------------------------------------------------------//
+
+    let rt_add: FunctionVariation = FunctionVariation::Native(|vals| {
+        let vals = if let NativeFunctionParameters::Singles(v) = vals { v } else { panic!("A native function with Singles function parameter type should not be able to receive a Variadic function parameter type") };
+        if vals.len() != 2 { return Err(runtime_error("add() expects 2 arguments".to_string())); }
+        let lhs = vals[0].clone();
+        let rhs = vals[1].clone();
+        if lhs.get_type().subtype(TY_INT) && rhs.get_type().subtype(TY_INT) {
+            let lhs_val = if let Value::Number(Number::SignedInteger(SignedInteger::Int32(v)), _) = lhs { v } else { panic!("add() expects 2 arguments of type '{}'", TY_INT) };
+            let rhs_val = if let Value::Number(Number::SignedInteger(SignedInteger::Int32(v)), _) = rhs { v } else { panic!("add() expects 2 arguments of type '{}'", TY_INT) };
+            Ok(Value::Number(Number::SignedInteger(SignedInteger::Int32(lhs_val + rhs_val)), TY_INT))
+        } else {
+            Err(runtime_error(format!("add() expects 2 arguments of type '{}', got '{:?}' and '{:?}'", TY_INT, lhs.get_type(), rhs.get_type())))
+        }
+    }, FunctionParameterType::Singles(vec![("lhs".to_string(), TY_INT), ("rhs".to_string(), TY_INT)]), TY_INT);
+
+//--------------------------------------------------------------------------------------//
+//                                   Implementations                                    //
+//--------------------------------------------------------------------------------------//
+
+    // Built-in operators, these are not overloadable and are reserved for the language
+    add_static(lexer, "assign", "=", OperatorPosition::Infix, 100, OperatorAssociativity::Right, false, assign_handler);
+    add_runtime(lexer, "add", "+", OperatorPosition::Infix, 100, OperatorAssociativity::Left, false, Box::new(rt_add));
+}
+
+
+//--------------------------------------------------------------------------------------//
+//                                                                                      //
+//                                   Init Environment                                   //
+//                                                                                      //
+//--------------------------------------------------------------------------------------//
+
+
+pub fn init_environment(env: &mut Environment) -> Result<(), RuntimeError> {
+    let add_value = |env: &mut Environment, name: &str, val: Value| {
+        env.add_value(Str::String(name.to_string()), val)
+    };
+    let add_func = |env: &mut Environment, name: &str, variations: Vec<FunctionVariation>| {
+        add_value(env, name, Value::Function(Function::new(name.to_string(), variations)))
+    };
+
+//--------------------------------------------------------------------------------------//
+//                               Native Runtime Functions                               //
+//--------------------------------------------------------------------------------------//
+
+    let rt_print: FunctionVariation = FunctionVariation::Native(|vals| {
+        let vals = if let NativeFunctionParameters::Variadic(_, v) = vals { v }
+        else { panic!("A native function with Variadic function parameter type should not be able to receive a Singles function parameter type") };
+        for val in vals { println!("{}", val); }
+        Ok(Value::Unit)
+    }, FunctionParameterType::Variadic(None, ("params".to_string(), TY_ANY)), TY_UNIT);
+
+//--------------------------------------------------------------------------------------//
+//                                      Constants                                       //
+//--------------------------------------------------------------------------------------//
+
+    add_value(env, "true", Value::Boolean(true))?;
+    add_value(env, "false", Value::Boolean(false))?;
+
+//--------------------------------------------------------------------------------------//
+//                                   Implementations                                    //
+//--------------------------------------------------------------------------------------//
+
+    add_func(env, "print", vec![rt_print])?;
+    Ok(())
+}
