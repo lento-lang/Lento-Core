7e196a9f43ceef93b6169c9a137501ac1bed21cd
William Rågstad <william.ragstad@gmail.com>
2023-01-24 20:58:43 +0100

    Add utility Str

---
+++ src/util/str.rs
@@ -0,0 +1,29 @@
+use std::fmt::Display;
+
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Str {
+    String(String),
+    Str(&'static str)
+}
+
+impl Display for Str {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Str::String(s) => s.fmt(f),
+            Str::Str(s) => s.fmt(f)
+        }
+    }
+}
+
+impl From<String> for Str {
+    fn from(s: String) -> Self {
+        Str::String(s)
+    }
+}
+
+impl From<&'static str> for Str {
+    fn from(s: &'static str) -> Self {
+        Str::Str(s)
+    }
+}
