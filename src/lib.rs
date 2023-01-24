97e57556277aaeafa2afe1ee13339bc6ac7e424d
William Rågstad <william.ragstad@gmail.com>
2023-01-24 20:59:28 +0100

    Update crate lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 pub mod conf;
 pub mod lexer;
 pub mod parser;
+pub mod type_checker;
 pub mod interpreter;
 pub mod compiler;
 pub mod project;
 pub mod printer;
 pub mod doc;
+pub mod stdlib;
+pub mod util;
 
 fn _add(left: usize, right: usize) -> usize {
     left + right
