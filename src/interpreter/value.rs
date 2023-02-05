f7dcff967feb2b23d3e547f8716773e37fd79d6d
William Rågstad <william.ragstad@gmail.com>
2023-02-05 18:09:32 +0100

    Implement advanced numerical casting and arithmetic ops

--- src/interpreter/value.rs
+++ src/interpreter/value.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Display, cmp::Ordering};
+use std::{collections::HashMap, fmt::Display, cmp::Ordering, ops::Add};
 
 use num_bigfloat::BigFloat;
 use num_bigint::{BigInt, BigUint};
@@ -6,6 +6,20 @@ use num_bigint::{BigInt, BigUint};
 use crate::{type_checker::types::{Type, FunctionParameterType, std_primitive_types, GetType, CheckedType}, parser::ast::Ast};
 
 use super::interpreter::InterpretResult;
+
+pub trait NumberCasting<T> {
+    fn up_cast_type(&self, to_reference_size: &T) -> T;
+    fn try_down_cast_ref(&self, to_reference_size: &T) -> Option<T>;
+    fn cast_to_ref(&self, to_reference_size: &T) -> Option<T>;
+    fn down_cast_max(&self) -> T;
+}
+
+pub trait ArithmeticOperations<T> {
+    fn add(lhs: &T, rhs: &T) -> T;
+    fn sub(lhs: &T, rhs: &T) -> T;
+    fn mul(lhs: &T, rhs: &T) -> T;
+    fn div(lhs: &T, rhs: &T) -> T;
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum UnsignedInteger {
@@ -18,6 +32,223 @@ pub enum UnsignedInteger {
     UIntVar(BigUint), // Arbitrary sized integers (variable size)
 }
 
+impl UnsignedInteger {
+    fn get_size_order(&self) -> u8 {
+        match self {
+            UnsignedInteger::UInt1(_) => 0,
+            UnsignedInteger::UInt8(_) => 1,
+            UnsignedInteger::UInt16(_) => 2,
+            UnsignedInteger::UInt32(_) => 3,
+            UnsignedInteger::UInt64(_) => 4,
+            UnsignedInteger::UInt128(_) => 5,
+            UnsignedInteger::UIntVar(_) => 6,
+        }
+    }
+
+    fn is_biguint(&self) -> bool {
+        match self {
+            UnsignedInteger::UIntVar(_) => true,
+            _ => false
+        }
+    }
+
+    fn get_u128_value(&self) -> Option<u128> {
+        Some(match self {
+            UnsignedInteger::UInt1(v) => *v as u128,
+            UnsignedInteger::UInt8(v) => *v as u128,
+            UnsignedInteger::UInt16(v) => *v as u128,
+            UnsignedInteger::UInt32(v) => *v as u128,
+            UnsignedInteger::UInt64(v) => *v as u128,
+            UnsignedInteger::UInt128(v) => *v,
+            UnsignedInteger::UIntVar(v) => return None
+        })
+    }
+
+    fn get_biguint_value(&self) -> Option<BigUint> {
+        Some(match self {
+            UnsignedInteger::UInt1(v) => BigUint::from(*v),
+            UnsignedInteger::UInt8(v) => BigUint::from(*v),
+            UnsignedInteger::UInt16(v) => BigUint::from(*v),
+            UnsignedInteger::UInt32(v) => BigUint::from(*v),
+            UnsignedInteger::UInt64(v) => BigUint::from(*v),
+            UnsignedInteger::UInt128(v) => BigUint::from(*v),
+            UnsignedInteger::UIntVar(v) => v.clone()
+        })
+    }
+}
+
+impl NumberCasting<UnsignedInteger> for UnsignedInteger {
+    /// Upcasts the value to the given reference size
+    /// # Arguments
+    /// * `to_reference_size` - The reference type size to cast to
+    /// # Note
+    /// This function does not check if the reference size is smaller or equal to the current size
+    /// This is the responsibility of the caller
+    fn up_cast_type(&self, to_reference_size: &UnsignedInteger) -> UnsignedInteger {
+        match (self, to_reference_size) {
+            (UnsignedInteger::UInt1(v), UnsignedInteger::UInt8(_)) => UnsignedInteger::UInt8(*v as u8),
+            (UnsignedInteger::UInt1(v), UnsignedInteger::UInt16(_)) => UnsignedInteger::UInt16(*v as u16),
+            (UnsignedInteger::UInt1(v), UnsignedInteger::UInt32(_)) => UnsignedInteger::UInt32(*v as u32),
+            (UnsignedInteger::UInt1(v), UnsignedInteger::UInt64(_)) => UnsignedInteger::UInt64(*v as u64),
+            (UnsignedInteger::UInt1(v), UnsignedInteger::UInt128(_)) => UnsignedInteger::UInt128(*v as u128),
+            (UnsignedInteger::UInt1(v), UnsignedInteger::UIntVar(_)) => UnsignedInteger::UIntVar(BigUint::from(*v)),
+            (UnsignedInteger::UInt8(v), UnsignedInteger::UInt16(_)) => UnsignedInteger::UInt16(*v as u16),
+            (UnsignedInteger::UInt8(v), UnsignedInteger::UInt32(_)) => UnsignedInteger::UInt32(*v as u32),
+            (UnsignedInteger::UInt8(v), UnsignedInteger::UInt64(_)) => UnsignedInteger::UInt64(*v as u64),
+            (UnsignedInteger::UInt8(v), UnsignedInteger::UInt128(_)) => UnsignedInteger::UInt128(*v as u128),
+            (UnsignedInteger::UInt8(v), UnsignedInteger::UIntVar(_)) => UnsignedInteger::UIntVar(BigUint::from(*v)),
+            (UnsignedInteger::UInt16(v), UnsignedInteger::UInt32(_)) => UnsignedInteger::UInt32(*v as u32),
+            (UnsignedInteger::UInt16(v), UnsignedInteger::UInt64(_)) => UnsignedInteger::UInt64(*v as u64),
+            (UnsignedInteger::UInt16(v), UnsignedInteger::UInt128(_)) => UnsignedInteger::UInt128(*v as u128),
+            (UnsignedInteger::UInt16(v), UnsignedInteger::UIntVar(_)) => UnsignedInteger::UIntVar(BigUint::from(*v)),
+            (UnsignedInteger::UInt32(v), UnsignedInteger::UInt64(_)) => UnsignedInteger::UInt64(*v as u64),
+            (UnsignedInteger::UInt32(v), UnsignedInteger::UInt128(_)) => UnsignedInteger::UInt128(*v as u128),
+            (UnsignedInteger::UInt32(v), UnsignedInteger::UIntVar(_)) => UnsignedInteger::UIntVar(BigUint::from(*v)),
+            (UnsignedInteger::UInt64(v), UnsignedInteger::UInt128(_)) => UnsignedInteger::UInt128(*v as u128),
+            (UnsignedInteger::UInt64(v), UnsignedInteger::UIntVar(_)) => UnsignedInteger::UIntVar(BigUint::from(*v)),
+            (UnsignedInteger::UInt128(v), UnsignedInteger::UIntVar(_)) => UnsignedInteger::UIntVar(BigUint::from(*v)),
+            _ => panic!("Cannot upcast type {:?} to {:?}", self, to_reference_size), // Unreachable
+        }
+    }
+
+    /// Try to downcast the type of the UnsignedInteger to the type of the reference.
+    /// Returns None if the downcast is not possible (value is too large to fit in the reference type)
+    /// Returns Some(UnsignedInteger) if the downcast is possible
+    /// # Arguments
+    /// * `to_reference_size` - The reference type size to cast to
+    /// # Note
+    /// This function does not check if the reference type is smaller or equal to the current type.
+    /// This is the responsibility of the caller.
+    fn try_down_cast_ref(&self, to_reference_size: &UnsignedInteger) -> Option<UnsignedInteger> {
+        match (self, to_reference_size) {
+            (UnsignedInteger::UInt8(v), UnsignedInteger::UInt1(_)) => if *v <= 1 { Some(UnsignedInteger::UInt1(*v as u8)) } else { None },
+            (UnsignedInteger::UInt16(v), UnsignedInteger::UInt1(_)) => if *v <= 1 { Some(UnsignedInteger::UInt1(*v as u8)) } else { None },
+            (UnsignedInteger::UInt32(v), UnsignedInteger::UInt1(_)) => if *v <= 1 { Some(UnsignedInteger::UInt1(*v as u8)) } else { None },
+            (UnsignedInteger::UInt64(v), UnsignedInteger::UInt1(_)) => if *v <= 1 { Some(UnsignedInteger::UInt1(*v as u8)) } else { None },
+            (UnsignedInteger::UInt128(v), UnsignedInteger::UInt1(_)) => if *v <= 1 { Some(UnsignedInteger::UInt1(*v as u8)) } else { None },
+            (UnsignedInteger::UIntVar(v), UnsignedInteger::UInt1(_)) =>
+                if v.le(&BigUint::from(1u8)) { Some(UnsignedInteger::UInt1(v.iter_u32_digits().nth(0).unwrap() as u8)) } else { None },
+            (UnsignedInteger::UInt16(v), UnsignedInteger::UInt8(_)) => if *v <= u8::MAX as u16 { Some(UnsignedInteger::UInt8(*v as u8)) } else { None },
+            (UnsignedInteger::UInt32(v), UnsignedInteger::UInt8(_)) => if *v <= u8::MAX as u32 { Some(UnsignedInteger::UInt8(*v as u8)) } else { None },
+            (UnsignedInteger::UInt64(v), UnsignedInteger::UInt8(_)) => if *v <= u8::MAX as u64 { Some(UnsignedInteger::UInt8(*v as u8)) } else { None },
+            (UnsignedInteger::UInt128(v), UnsignedInteger::UInt8(_)) => if *v <= u8::MAX as u128 { Some(UnsignedInteger::UInt8(*v as u8)) } else { None },
+            (UnsignedInteger::UIntVar(v), UnsignedInteger::UInt8(_)) =>
+                if v.le(&BigUint::from(u8::MAX)) { Some(UnsignedInteger::UInt8(v.iter_u32_digits().nth(0).unwrap() as u8)) } else { None },
+            (UnsignedInteger::UInt32(v), UnsignedInteger::UInt16(_)) => if *v <= u16::MAX as u32 { Some(UnsignedInteger::UInt16(*v as u16)) } else { None },
+            (UnsignedInteger::UInt64(v), UnsignedInteger::UInt16(_)) => if *v <= u16::MAX as u64 { Some(UnsignedInteger::UInt16(*v as u16)) } else { None },
+            (UnsignedInteger::UInt128(v), UnsignedInteger::UInt16(_)) => if *v <= u16::MAX as u128 { Some(UnsignedInteger::UInt16(*v as u16)) } else { None },
+            (UnsignedInteger::UIntVar(v), UnsignedInteger::UInt16(_)) =>
+                if v.le(&BigUint::from(u16::MAX)) { Some(UnsignedInteger::UInt16(v.iter_u32_digits().nth(0).unwrap() as u16)) } else { None },
+            (UnsignedInteger::UInt64(v), UnsignedInteger::UInt32(_)) => if *v <= u32::MAX as u64 { Some(UnsignedInteger::UInt32(*v as u32)) } else { None },
+            (UnsignedInteger::UInt128(v), UnsignedInteger::UInt32(_)) => if *v <= u32::MAX as u128 { Some(UnsignedInteger::UInt32(*v as u32)) } else { None },
+            (UnsignedInteger::UIntVar(v), UnsignedInteger::UInt32(_)) =>
+                if v.le(&BigUint::from(u32::MAX)) { Some(UnsignedInteger::UInt32(v.iter_u32_digits().nth(0).unwrap() as u32)) } else { None },
+            (UnsignedInteger::UInt128(v), UnsignedInteger::UInt64(_)) => if *v <= u64::MAX as u128 { Some(UnsignedInteger::UInt64(*v as u64)) } else { None },
+            (UnsignedInteger::UIntVar(v), UnsignedInteger::UInt64(_)) =>
+                if v.le(&BigUint::from(u64::MAX)) { Some(UnsignedInteger::UInt64(v.iter_u32_digits().nth(0).unwrap() as u64)) } else { None },
+            (UnsignedInteger::UIntVar(v), UnsignedInteger::UInt128(_)) =>
+                if v.le(&BigUint::from(u128::MAX)) { Some(UnsignedInteger::UInt128(v.iter_u32_digits().nth(0).unwrap() as u128)) } else { None },
+            _ => panic!("Cannot downcast type {:?} to {:?}", self, to_reference_size)
+        }
+    }
+
+    /// Casts the current type to the reference type, if possible.
+    /// If the current type is smaller than the reference type, it will be upcasted.
+    /// If the current type is larger than the reference type, it will be downcasted.
+    /// If the current type is equal to the reference type, no casting will be performed.
+    /// If the current type is larger than the reference type and cannot be downcasted, None will be returned.
+    /// # Arguments
+    /// * `to_reference_size` - The reference type size to cast to
+    /// # Returns
+    /// The casted type, if possible
+    /// # Panics
+    /// If the current type is larger than the reference type and cannot be downcasted
+    fn cast_to_ref(&self, to_reference_size: &UnsignedInteger) -> Option<UnsignedInteger> {
+        let cmp_res = self.get_size_order().cmp(&to_reference_size.get_size_order());
+        if cmp_res == Ordering::Equal { Some(self.clone()) } // No casting needed
+        // self < reference, upcast to the reference size
+        else if cmp_res == Ordering::Less { Some(self.up_cast_type(to_reference_size)) }
+        // self > reference, try to downcast to the reference size
+        else { self.try_down_cast_ref(to_reference_size) }
+    }
+
+    /// Returns the smallest possible type that can hold the current value without loss of precision or overflow.
+    fn down_cast_max(&self) -> UnsignedInteger {
+        match self {
+            UnsignedInteger::UInt1(_) => self.clone(),
+            UnsignedInteger::UInt8(v) => if *v <= 1 { UnsignedInteger::UInt1(*v) } else { self.clone() },
+            UnsignedInteger::UInt16(v) => {
+                if *v <= 1 { UnsignedInteger::UInt1(*v as u8) }
+                else if *v <= u8::MAX as u16 { UnsignedInteger::UInt8(*v as u8) }
+                else { self.clone() }
+            },
+            UnsignedInteger::UInt32(v) => {
+                if *v <= 1 { UnsignedInteger::UInt1(*v as u8) }
+                else if *v <= u8::MAX as u32 { UnsignedInteger::UInt8(*v as u8) }
+                else if *v <= u16::MAX as u32 { UnsignedInteger::UInt16(*v as u16) }
+                else { self.clone() }
+            },
+            UnsignedInteger::UInt64(v) => {
+                if *v <= 1 { UnsignedInteger::UInt1(*v as u8) }
+                else if *v <= u8::MAX as u64 { UnsignedInteger::UInt8(*v as u8) }
+                else if *v <= u16::MAX as u64 { UnsignedInteger::UInt16(*v as u16) }
+                else if *v <= u32::MAX as u64 { UnsignedInteger::UInt32(*v as u32) }
+                else { self.clone() }
+            },
+            UnsignedInteger::UInt128(v) => {
+                if *v <= 1 { UnsignedInteger::UInt1(*v as u8) }
+                else if *v <= u8::MAX as u128 { UnsignedInteger::UInt8(*v as u8) }
+                else if *v <= u16::MAX as u128 { UnsignedInteger::UInt16(*v as u16) }
+                else if *v <= u32::MAX as u128 { UnsignedInteger::UInt32(*v as u32) }
+                else if *v <= u64::MAX as u128 { UnsignedInteger::UInt64(*v as u64) }
+                else { self.clone() }
+            },
+            UnsignedInteger::UIntVar(v) => {
+                if v.le(&BigUint::from(1u8)) { UnsignedInteger::UInt1(v.iter_u32_digits().nth(0).unwrap() as u8) }
+                else if v.le(&BigUint::from(u8::MAX)) { UnsignedInteger::UInt8(v.iter_u32_digits().nth(0).unwrap() as u8) }
+                else if v.le(&BigUint::from(u16::MAX)) { UnsignedInteger::UInt16(v.iter_u32_digits().nth(0).unwrap() as u16) }
+                else if v.le(&BigUint::from(u32::MAX)) { UnsignedInteger::UInt32(v.iter_u32_digits().nth(0).unwrap() as u32) }
+                else if v.le(&BigUint::from(u64::MAX)) { UnsignedInteger::UInt64(v.iter_u32_digits().nth(0).unwrap() as u64) }
+                else if v.le(&BigUint::from(u128::MAX)) { UnsignedInteger::UInt128(v.iter_u32_digits().nth(0).unwrap() as u128) }
+                else { self.clone() }
+            }
+        }
+    }
+}
+
+impl ArithmeticOperations<UnsignedInteger> for UnsignedInteger {
+    fn add(lhs: &UnsignedInteger, rhs: &UnsignedInteger) -> UnsignedInteger {
+        // Always perform arithmetic operations on u128, then cast back to the smallest possible type
+        if lhs.is_biguint() || rhs.is_biguint() {
+            let lhs = lhs.get_biguint_value().unwrap();
+            let rhs = rhs.get_biguint_value().unwrap();
+            UnsignedInteger::UIntVar(BigUint::add(lhs, rhs))
+        } else {
+            let lhs = lhs.get_u128_value().unwrap();
+            let rhs = rhs.get_u128_value().unwrap();
+            if let Some(res) = lhs.checked_add(rhs) {
+                UnsignedInteger::UInt128(res).down_cast_max()
+            } else {
+                let lhs = BigUint::from(lhs);
+                let rhs = BigUint::from(rhs);
+                UnsignedInteger::UIntVar(BigUint::add(lhs, rhs))
+            }
+        }
+    }
+
+    fn sub(lhs: &UnsignedInteger, rhs: &UnsignedInteger) -> UnsignedInteger {
+        todo!()
+    }
+
+    fn mul(lhs: &UnsignedInteger, rhs: &UnsignedInteger) -> UnsignedInteger {
+        todo!()
+    }
+
+    fn div(lhs: &UnsignedInteger, rhs: &UnsignedInteger) -> UnsignedInteger {
+        todo!()
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SignedInteger {
     Int8(i8),
@@ -28,12 +259,48 @@ pub enum SignedInteger {
     IntVar(BigInt), // Arbitrary sized integers (variable size)
 }
 
+impl ArithmeticOperations<SignedInteger> for SignedInteger {
+    fn add(lhs: &SignedInteger, rhs: &SignedInteger) -> SignedInteger {
+        todo!()
+    }
+
+    fn sub(lhs: &SignedInteger, rhs: &SignedInteger) -> SignedInteger {
+        todo!()
+    }
+
+    fn mul(lhs: &SignedInteger, rhs: &SignedInteger) -> SignedInteger {
+        todo!()
+    }
+
+    fn div(lhs: &SignedInteger, rhs: &SignedInteger) -> SignedInteger {
+        todo!()
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum FloatingPoint {
     Float32(f32),
     Float64(f64),
     FloatBig(BigFloat), // Increased precision floating point numbers (fixed but large size)
     // Look at https://github.com/stencillogic/astro-float for handling Arbitrary precision floating point numbers (variable size)
+}
+
+impl ArithmeticOperations<FloatingPoint> for FloatingPoint {
+    fn add(lhs: &FloatingPoint, rhs: &FloatingPoint) -> FloatingPoint {
+        todo!()
+    }
+
+    fn sub(lhs: &FloatingPoint, rhs: &FloatingPoint) -> FloatingPoint {
+        todo!()
+    }
+
+    fn mul(lhs: &FloatingPoint, rhs: &FloatingPoint) -> FloatingPoint {
+        todo!()
+    }
+
+    fn div(lhs: &FloatingPoint, rhs: &FloatingPoint) -> FloatingPoint {
+        todo!()
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -45,7 +312,7 @@ pub enum Number {
 
 impl GetType for Number {
     fn get_type(&self) -> CheckedType {
-        Some(match self {
+        CheckedType::Checked(match self {
             Number::UnsignedInteger(u) => match u {
                 UnsignedInteger::UInt1(_) => std_primitive_types::UINT1,
                 UnsignedInteger::UInt8(_) => std_primitive_types::UINT8,
@@ -115,22 +382,45 @@ impl Number {
                     Number::SignedInteger(SignedInteger::Int128(i as i128))
                 }
             } else {
-                let u = s.parse::<i128>();
+                let u = s.parse::<u128>();
                 if u.is_err() { return Number::parse_big_uint(s); }
                 let u = u.unwrap();
-                if u >= std::u8::MIN as i128 && u <= std::u8::MAX as i128 {
+                if u >= std::u8::MIN as u128 && u <= std::u8::MAX as u128 {
                     Number::UnsignedInteger(UnsignedInteger::UInt8(u as u8))
-                } else if u >= std::u16::MIN as i128 && u <= std::u16::MAX as i128 {
+                } else if u >= std::u16::MIN as u128 && u <= std::u16::MAX as u128 {
                     Number::UnsignedInteger(UnsignedInteger::UInt16(u as u16))
-                } else if u >= std::u32::MIN as i128 && u <= std::u32::MAX as i128 {
+                } else if u >= std::u32::MIN as u128 && u <= std::u32::MAX as u128 {
                     Number::UnsignedInteger(UnsignedInteger::UInt32(u as u32))
-                } else if u >= std::u64::MIN as i128 && u <= std::u64::MAX as i128 {
+                } else if u >= std::u64::MIN as u128 && u <= std::u64::MAX as u128 {
                     Number::UnsignedInteger(UnsignedInteger::UInt64(u as u64))
                 } else {
                     Number::UnsignedInteger(UnsignedInteger::UInt128(u as u128))
                 }
             }
         }
+    }
+}
+
+impl ArithmeticOperations<Number> for Number {
+    fn add(lhs: &Number, rhs: &Number) -> Number {
+        match (lhs, rhs) {
+            (Number::UnsignedInteger(lhs), Number::UnsignedInteger(rhs)) => Number::UnsignedInteger(UnsignedInteger::add(lhs, rhs)),
+            (Number::SignedInteger(lhs), Number::SignedInteger(rhs)) => todo!(),
+            (Number::FloatingPoint(lhs), Number::FloatingPoint(rhs)) => todo!(),
+            _ => todo!()
+        }
+    }
+
+    fn sub(lhs: &Number, rhs: &Number) -> Number {
+        todo!()
+    }
+
+    fn mul(lhs: &Number, rhs: &Number) -> Number {
+        todo!()
+    }
+
+    fn div(lhs: &Number, rhs: &Number) -> Number {
+        todo!()
     }
 }
 
@@ -197,7 +487,7 @@ impl FunctionVariation {
 
 impl GetType for FunctionVariation {
     fn get_type(&self) -> CheckedType {
-        Some(match self {
+        CheckedType::Checked(match self {
             FunctionVariation::User(p, _, r) => Type::Function(Box::new(p.clone()), Box::new(r.clone())),
             FunctionVariation::Native(_, v, r) => Type::Function(Box::new(v.clone()), Box::new(r.clone())),
         })
@@ -249,7 +539,7 @@ pub enum Value {
 
 impl GetType for Value {
     fn get_type(&self) -> CheckedType {
-        Some(match self {
+        CheckedType::Checked(match self {
             Value::Unit => Type::Unit,
             Value::Number(n) => {
                 match n {
