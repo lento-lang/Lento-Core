112593be40a7fbe48e0b95b7e67ea0902903e32c
William Rågstad <william.ragstad@gmail.com>
2023-02-05 18:06:56 +0100

    Use `simplify`

--- src/type_checker/types.rs
+++ src/type_checker/types.rs
@@ -266,7 +266,7 @@ impl Type {
 
 impl Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
+        match self.simplify() {
             Type::Any => write!(f, "any"),
             Type::Unit => write!(f, "()"),
             Type::Literal(t) => write!(f, "{}", t),
