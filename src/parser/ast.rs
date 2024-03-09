461207a7ff50e1ad5b4c000ec21c32c4478c4968
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2024-03-09 20:21:35 +0100

    Add documentation to AST

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -37,21 +37,59 @@ impl Module {
  */
 #[derive(Debug, Clone, PartialEq)]
 pub enum Ast {
-    /// Primitive values such as Integer, Float, String, Char and Boolean (NOT compound data structures)
+    /// A literal is a constant value that is directly represented in the source code.
+    /// 1. Value of the literal
     Literal(Value),
     /// A tuple is a fixed-size collection of elements of possibly different types.
+    /// 1. List of elements
+    /// 2. Type of the tuple, made up of the types of the elements and the number of elements
     Tuple(Vec<Ast>, CheckedType),
-    /// A dynamic list of elements of the same type.
+    /// A dynamic list of elements.
+    /// 1. List of elements
+    /// 2. Type of every element in the list (all elements must **be a subtype**)
     List(Vec<Ast>, CheckedType),
+    /// A record is a collection of key-value pairs
+    /// 1. List of key-value pairs
+    /// 2. Type of the record, made up of the types of the keys and values
     Record(HashMap<RecordKeyAst, Ast>, CheckedType),
+    /// An identifier is a named reference to a value in the environment
+    /// 1. Name of the identifier
+    /// 2. Type of the identifier (the type of the value it refers to)
     Identifier(String, CheckedType),
+    /// A type identifier is a named reference to a type in the environment
+    /// 1. Name of the type identifier
+    /// 2. Type definition of the type identifier
     TypeIdentifier(String, CheckedType),
+    /// A function call is an invocation of a function with a list of arguments
+    /// 1. Name of the function
+    /// 2. List of arguments
+    /// 3. Type of the return value of the function
     FunctionCall(String, Vec<Ast>, CheckedType),
+    /// A function definition is a named function with a list of parameters and a body expression
+    /// 1. Name of the function
+    /// 2. List of parameters
+    /// 3. Body expression
+    /// 4. Type of the return value of the function
     Function(String, FunctionParameterType, Box<Ast>, CheckedType),
+    /// A binary expression is an operation with two operands
+    /// 1. Left operand
+    /// 2. Operator
+    /// 3. Right operand
+    /// 4. Type of the result of the operation
     Binary(Box<Ast>, Operator, Box<Ast>, CheckedType),
+    /// A unary expression is an operation with one operand
+    /// 1. Operator
+    /// 2. Operand
+    /// 3. Type of the result of the operation
     Unary(Operator, Box<Ast>, CheckedType),
+    /// An assignment expression assigns a value to a variable
+    /// 1. Matching pattern (identifier, destructuring of a tuple, record, etc.)
+    /// 2. Value
+    /// 3. Type of the value
     Assignment(Box<Ast>, Box<Ast>, CheckedType),
     /// Block expression evaluates all expressions in the block and returns the value of the last expression.
+    /// 1. List of expressions
+    /// 2. Type of the last expression
     Block(Vec<Ast>, CheckedType),
 }
 
