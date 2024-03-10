3e2e5a0b56d077971dab9654bf1b08f6021069ba
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2024-03-10 22:32:31 +0100

    Add optimization level to compiler options

--- src/compiler/compiler.rs
+++ src/compiler/compiler.rs
@@ -10,15 +10,43 @@ use target_lexicon::Triple;
 //                                      Compiler                                        //
 //--------------------------------------------------------------------------------------//
 
+/// Optimization level for the compiler. \
+/// The optimization level is used to specify the amount of optimization to perform when compiling.
+pub enum OptimizationLevel {
+    None,
+    Low,
+    Medium,
+    High,
+    Max,
+}
+
 /// Generic *(backend-agnostic)* options for the compiler. \
 /// The options are used to configure the compiler's behavior
 /// when compiling a module.
 pub struct CompileOptions {
-    pub optimization_level: u8,
+    pub optimization_level: OptimizationLevel,
     pub debug_info: bool,
     pub target: Triple,
     pub output_file: PathBuf,
     pub input_source: InputSource,
+}
+
+impl CompileOptions {
+    pub fn new(
+        optimization_level: OptimizationLevel,
+        debug_info: bool,
+        target: Triple,
+        output_file: PathBuf,
+        input_source: InputSource,
+    ) -> Self {
+        Self {
+            optimization_level,
+            debug_info,
+            target,
+            output_file,
+            input_source,
+        }
+    }
 }
 
 /// The result of compiling a module. \
